c38945eeb10e68df21a8ff06fb279eb243e40155
yelsinsotelo <36383209+yelsinsotelo@users.noreply.github.com>
2018-07-24 15:06:03 -0500

    Create avrpin.h
    description de pines

---
+++ avrpin.h
@@ -0,0 +1,21 @@
+/*
+ * avrpin.h
+ *
+ * Created: 24/07/2018 10:17:56 a.m.
+ *  Author: INGENIERIA-05
+ */ 
+
+
+#ifndef AVRPIN_H_
+#define AVRPIN_H_
+#define pinADC0 0b0000
+#define pinADC1 0b0001
+#define pinADC2 0b0010
+#define pinADC3 0b0011
+#define pinADC4 0b0100
+#define pinADC5 0b0101
+#define pinADC6 0b0110
+#define pinADC7 0b0111
+#define delay(ms) _delay_ms((ms))
+
+#endif /* AVRPIN_H_ */
